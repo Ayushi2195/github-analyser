0c4d957e10dfa6b44bf5056de2c46347e30524be
Ayushi <ayushigondwal@gmail.com>
2026-06-12 02:32:15 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -142,3 +142,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
+    if origin.strip()
+]
